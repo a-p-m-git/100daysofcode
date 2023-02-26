79ed7864d598bb3239b11ab458081f5477d90a21
apm <github@matlock.ca>
2023-02-26 14:21:54 -0400

    day8 challenge 4 complete

--- 08caesarCipher/caesarCipher4.py
+++ 08caesarCipher/caesarCipher4.py
@@ -1,18 +1,18 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+running = True
 
 def caesar(text, shift, action):
     encoded = ""
-    
+
     if action == "encode":
         for letter in text:
-            print(f"{alphabet.index(letter)} / {shift} / {alphabet.index(letter) - shift} / {len(alphabet)}")
-            if (alphabet.index(letter) + shift) >= len(alphabet):
+            #print(f"{alphabet.index(letter)} / {shift} / {alphabet.index(letter) - shift} / {len(alphabet)}")
+            if not (alphabet.count(letter)):
+                print(letter)
+                encoded += letter
+            elif (alphabet.index(letter) + shift) >= len(alphabet):
                 newShift = alphabet.index(letter) - shift
                 #print(f"{letter} shifted {shift} - {newShift} {alphabet.index(letter) - shift} will put it outside bounds of alphabet index")
                 #print(f"{alphabet[alphabet.index(letter)]} -> {alphabet[newShift]}")
@@ -23,7 +23,10 @@ def caesar(text, shift, action):
     elif action == "decode":
         for letter in text:
             #print(f"{alphabet.index(letter)} / {shift} / {alphabet.index(letter) - shift} / {len(alphabet)}")
-            if (alphabet.index(letter) - shift) <= 0:
+            if not (alphabet.count(letter)):
+                print(letter)
+                encoded += letter
+            elif (alphabet.index(letter) - shift) <= 0:
                 newShift = alphabet.index(letter) - shift
              #   print(f"{letter} shifted {shift} - {newShift} will put it outside bounds of alphabet index")
              #   print(f"{alphabet[alphabet.index(letter)]} -> {alphabet[alphabet.index(letter) + shift]}")
@@ -34,8 +37,19 @@ def caesar(text, shift, action):
 
     print(f"The encode text is {encoded}")
 
+while running:
+    print(art.logo)
     
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    
+    shift = int(input("Type the shift number:\n"))
 
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+    while shift < 0 or shift > 26:
+        print(f"{shift} value is invalid. Please choose a number between 1 and 26")
+        shift = int(input("Type the shift number: "))
 
-caesar(text,shift,direction)
+    caesar(text,shift,direction)
+    if input("Do you want to try again? [Y\\n]").lower() == 'n':
+        running=False
+            
