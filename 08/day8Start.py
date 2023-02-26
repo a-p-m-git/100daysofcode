aa429c98f64f99e509c9aadf1d3470fe76473a23
apm <apm@matlock.ca>
2023-02-25 21:22:04 -0400

    day8 partial

---
+++ 08/day8Start.py
@@ -0,0 +1,14 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet(name, password):
+    print(f"Good Day, {name}")
+    print(f"Your password is {password}")
+    print(".........")
+
+
+greet("paul","password")
+
