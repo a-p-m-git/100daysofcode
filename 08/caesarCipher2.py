890e9e5f7b26dd1a2587f2ff96e33964635bd030
apm <github@matlock.ca>
2023-02-26 09:47:59 -0400

    day8 caesar cipher decode complete.

--- 08/caesarCipher2.py
+++ 08/caesarCipher2.py
@@ -4,16 +4,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
 def encrypt(text, shift):
     encoded = ""
     
     for letter in text:
-        print(f"{alphabet.index(letter)} / {shift} / {alphabet.index(letter) + shift} / {len(alphabet)}")
+        print(f"{alphabet.index(letter)} / {shift} / {alphabet.index(letter) - shift} / {len(alphabet)}")
         if (alphabet.index(letter) + shift) >= len(alphabet):
-            newShift = (shift - (len(alphabet) - alphabet.index(letter)))
-            print(f"{letter} shifted {shift} - {newShift} will put it outside bounds of alphabet index")
+            newShift = alphabet.index(letter) - shift
+            print(f"{letter} shifted {shift} - {newShift} {alphabet.index(letter) - shift} will put it outside bounds of alphabet index")
+            print(f"{alphabet[alphabet.index(letter)]} -> {alphabet[newShift]}")
             encoded += alphabet[newShift]
         else:
             print(f"{alphabet[alphabet.index(letter)]} -> {alphabet[alphabet.index(letter) + shift]}")
@@ -21,17 +20,27 @@ def encrypt(text, shift):
 
     print(f"The encode text is {encoded}")
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+def decrypt(text, shift):
+    deCoded = ""
+    
+    for letter in text:
+        print(f"{alphabet.index(letter)} / {shift} / {alphabet.index(letter) - shift} / {len(alphabet)}")
+        if (alphabet.index(letter) - shift) <= 0:
+            newShift = alphabet.index(letter) - shift
+            print(f"{letter} shifted {shift} - {newShift} will put it outside bounds of alphabet index")
+            print(f"{alphabet[alphabet.index(letter)]} -> {alphabet[alphabet.index(letter) + shift]}")
+            deCoded += alphabet[newShift]
+        else:
+            print(f"{alphabet[alphabet.index(letter)]} -> {alphabet[alphabet.index(letter) - shift]}")
+            deCoded += alphabet[alphabet.index(letter) - shift]
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    print(f"The decoded text is {deCoded}")
+    
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text,shift)
+if direction == "encode":
+    encrypt(text,shift)
+else:
+    decrypt(text,shift)
