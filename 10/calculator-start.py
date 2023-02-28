478f3a4ffa10e63d9b9f55094973d067f629f544
apm <github@matlock.ca>
2023-02-28 16:24:21 -0400

    day10 misc

--- 10/calculator-start.py
+++ 10/calculator-start.py
@@ -27,14 +27,14 @@ operations = {
 
 running = True
 
+num1 = int(input("What's the first number?: "))
+
+for key in operations:
+    print(key)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
 while running:
-    
-    num1 = int(input("What's the first number?: "))
-
-    for key in operations:
-        print(key)
-
-    operation_symbol = input("Pick an operation from the line above: ")
 
     num2 = int(input("What's the next number? "))
 
@@ -43,7 +43,9 @@ while running:
     print(f"{num1} {operation_symbol} {num2} = {answer}")
 
     if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ").lower() == 'y':
-        pass
+        
+        num2 = int(input("What's the next number? "))
+
     else:
         running = False
         
