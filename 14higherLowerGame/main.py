56b0f41d7686762d41a024d2eae9aa56a5a15fe3
apm <github@matlock.ca>
2023-03-03 15:18:09 -0400

    day14 in progress

---
+++ 14higherLowerGame/main.py
@@ -0,0 +1,48 @@
+import art
+import game_data
+import random
+
+game_choices = {"A":[],"B":[]}
+
+def random_entry(data_dictionary):
+    return random.choice(data_dictionary)
+
+def print_entry(entry):
+    print(f"{entry['name']}, {entry['description']}, from {entry['country']} - {entry['follower_count']}")
+
+def compare_follower_counts(dictionaryA,dictionaryB):
+    if dictionaryA["follower_count"] > dictionaryB["follower_count"]:
+        return "A"
+    elif dictionaryA["follower_count"] < dictionaryB["follower_count"]:
+        return "B"
+
+def game():
+    
+    running = True
+    score = 0
+    
+    #set initial choices
+    game_choices["A"] = random_entry(game_data.data)
+    game_choices["B"] = random_entry(game_data.data)
+    print(art.logo)
+    
+    while running:
+        
+        
+        print_entry(game_choices["A"])
+        print(art.vs)
+        print_entry(game_choices["B"])
+
+        response = input("Who has more followers? Type 'A' or 'B' ").upper()
+
+        if response == compare_follower_counts(game_choices["A"],game_choices["B"]):
+            score += 1
+            print(art.logo)
+            print(f"You are right! Current Score is {score}")
+            game_choices["A"] = game_choices[response]
+            game_choices["B"] = random_entry(game_data.data)
+        else:
+            print(f"You are wrong. Final Score is {score}")
+            running = False
+
+game()
