b4b6a749d9af347ffb0a61c50b9b4740b329de18
apm <github@matlock.ca>
2023-03-14 15:42:52 -0300

    day18 challenge 2 in progress

---
+++ 18turtleAndGui/day-18-2-start.py
@@ -0,0 +1,15 @@
+import turtle
+
+#TODO-1 DRAW A DASHED LINE FOR 10 PACES AND THEN A GAP OF 10 PACES 50 TIMES
+
+turtle_screen = turtle.getscreen()
+tim_turtle = turtle.Turtle()
+tim_turtle.setx(100)
+tim_turtle.sety(100)
+print(tim_turtle.position())
+
+
+
+
+""" for i in range(50):
+  tim_turtle.forward(i) """
