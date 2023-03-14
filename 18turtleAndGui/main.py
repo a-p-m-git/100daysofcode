ea94de54ca1fcb32c2cef2bd230c28b76572f884
apm <github@matlock.ca>
2023-03-14 14:31:38 -0300

    day18 challenge 1 complete

---
+++ 18turtleAndGui/main.py
@@ -0,0 +1,17 @@
+import turtle
+import random
+
+#TODO-1 DRAW 100X100 SQUARE
+
+turtle_screen = turtle.getscreen()
+tim_turtle = turtle.Turtle()
+
+tim_turtle.forward(100)
+tim_turtle.right(90)
+tim_turtle.forward(100)
+tim_turtle.right(90)
+tim_turtle.forward(100)
+tim_turtle.right(90)
+tim_turtle.forward(100)
+
+
