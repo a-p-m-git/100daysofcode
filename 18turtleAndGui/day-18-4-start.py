d9584a448c0ccc57bfa7a78555abcecef9a3fd56
apm <github@matlock.ca>
2023-03-16 13:09:07 -0300

    day18 challenge 4 complete

--- 18turtleAndGui/day-18-4-start.py
+++ 18turtleAndGui/day-18-4-start.py
@@ -4,6 +4,8 @@ import random
 #TODO-1 Draw a triangle, square, pentagon, hexagon, heptagon, octagon, `
 # nonagon and decagon. Each shape has a random color and sides-length of 100
 
+running = True
+
 screen_width = 1024
 screen_height = 768
 
@@ -11,17 +13,23 @@ turtle_screen = t.Screen()
 turtle_screen.screensize(screen_width,screen_height)
 turtle_screen.colormode(255)
 tim_turtle = t.Turtle()
+tim_turtle.pensize(12)
 
-randomRGB = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-tim_turtle.pencolor(randomRGB)
-
-while turtle_screen.exitonclick():
-    
+while running:
     randomRGB = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-    tim_turtle.pencolor(randomRGB)
-    
     direction = random.randint(0,3)
     
     if direction == 0:
+        tim_turtle.setheading(90)
+    elif direction == 1:
+        tim_turtle.setheading(270)
+    elif direction == 2:
+        tim_turtle.setheading(0)
+    else:
+        tim_turtle.setheading(180)
+        
+    tim_turtle.pencolor(randomRGB)
+    tim_turtle.forward(50)
+
         
 
