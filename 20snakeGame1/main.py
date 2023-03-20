0412766c4d6230f7ff7d1698431d0e5be51f8bb7
apm <github@matlock.ca>
2023-03-20 14:09:56 -0300

    day 20 in prog

---
+++ 20snakeGame1/main.py
@@ -0,0 +1,30 @@
+import turtle
+import random
+
+
+
+
+s = turtle.Screen()
+s.setup(width=600,height=600)
+s.bgcolor("black")
+
+s.title("sNake 0.0")
+
+#TODO-1 CREATE SNAKE BODY
+
+#TODO-2 MOVE THE SNAKE
+
+#TODO-3 CREATE SNAKE FOOD
+
+#TODO-4 DETECT COLLISION WITH FOOD
+
+#TODO-5 CREATE A SCOREBOARD
+
+#TODO-6 DETECT COLLISION WITH WALL
+
+#TODO-7 DETECT COLLISION WITH TAIL
+
+
+
+
+s.exitonclick()
